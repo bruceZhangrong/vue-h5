2b65f2de85abf2b38d4b641d4f4ade67c69f14ce
zhangRong <bruce.zhang@supernovacompanies.biz>
2020-10-10 11:48:12 +0800

    修改remote

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div ref="app" class="full-screen bg-fff" @click.prevent="">
-    <router-view class="view flex-box" @click.prevent="" ></router-view>
+  <div ref="app" class="full-screen bg-fff" @click.prevent>
+    <router-view class="view flex-box" @click.prevent></router-view>
     <section v-if="footBar.isNavigate">
       <ul id="nav" class="pos-f text-center">
         <li>
-          <router-link  class="active" :class="{'RouterActive': firstTimes}" active-class="RouterActive" to="/home">
+          <router-link
+            class="active"
+            :class="{'RouterActive': firstTimes}"
+            active-class="RouterActive"
+            to="/home"
+          >
             <!-- <img :src="`./static/images/company${onIcon[0]}.png`" /> -->
             <i class="fa fa-2x fa-home"></i>
             <span>首页</span>
@@ -40,46 +45,45 @@
 </template>
 
 <style>
-  #nav {
-    bottom: 0;
-    background: rgb(83, 202, 196);
-    width: 100%;
-    display: flex;
-    justify-content: space-around;
-    font-size: 12px;
-    box-shadow: 0 -1px 10px #fff;
-  }
-  #nav li {
-    float: left;
-    flex: 1;
-  }
-  #nav img {
-    width: 38px;
-    height: 38px;
-    margin: 0 auto;
-  }
-  #nav a {
-    display: block;
-    color: #fff;
-    padding-top: 5px;
-    padding-bottom: 5px;
-  }
-  #nav a.RouterActive {
-    background: #fff;
-    color: rgb(83, 202, 196);
-  }
-  #bruce {
-    overflow-y: auto;
-  }
-  .view {
-    width: 100%;
-    height: 100%;
-  }
-  #nav i {
-    display: block;
-    margin-bottom: 5px;
-  }
-
+#nav {
+  bottom: 0;
+  background: rgb(83, 202, 196);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  font-size: 12px;
+  box-shadow: 0 -1px 10px #fff;
+}
+#nav li {
+  float: left;
+  flex: 1;
+}
+#nav img {
+  width: 38px;
+  height: 38px;
+  margin: 0 auto;
+}
+#nav a {
+  display: block;
+  color: #fff;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+#nav a.RouterActive {
+  background: #fff;
+  color: rgb(83, 202, 196);
+}
+#bruce {
+  overflow-y: auto;
+}
+.view {
+  width: 100%;
+  height: 100%;
+}
+#nav i {
+  display: block;
+  margin-bottom: 5px;
+}
 </style>
 
 <script>
